d6a602e8de18035c7cf21300150356d542ee74fe
irvin leon <irvinlucho@gmail.com>
2020-06-19 21:01:02 -0500

    open closed

---
+++ src/main/java/srp/models/Category.java
@@ -0,0 +1,32 @@
+package srp.models;
+
+
+class Category {
+    private String id;
+    private String name;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/srp/models/Family.java
@@ -0,0 +1,31 @@
+package srp.models;
+
+public class Family {
+    private String id;
+    private String name;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
